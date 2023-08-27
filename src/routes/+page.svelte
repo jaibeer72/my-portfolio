73c7e1e0c90c3958571a2f25ca7da297ec8b3644
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-08-27 13:56:12 +0100

    Testing if actions are scuessful

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <section class="text-center w-full h-screen bg-gradient-to-r from-yellow-200 via-red-300 to-pink-300">
     <h1 class="pt-10 text-7xl">Home Page</h1>
-    <div class="font-serif pt-12 text-2xl">wow</div>
+    <div class="font-serif pt-12 text-2xl">Actions are scuessful</div>
  </section>

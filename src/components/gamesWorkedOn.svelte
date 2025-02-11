1fd95471a9b78fb0b6f8b3664608176b2fd746b6
Jaibeer  S Dugal <jaibeer72@gmail.com>
2025-02-11 22:55:35 +0530

    added itch.io stuff

--- src/components/gamesWorkedOn.svelte
+++ src/components/gamesWorkedOn.svelte
@@ -2,39 +2,75 @@
     // Array of games with name, image URL, and website URL
     import JDPicture from "$lib/JD2023.jpeg";
     import ODDBALLERS from "$lib/logo-oddballers.webp";
+
     let games = [
-      {
-        name: "Oddballers",
-        imageUrl: ODDBALLERS, // replace with actual image URL
-        websiteUrl: "https://www.ubisoft.com/en-gb/game/oddballers" // replace with actual website URL
-      },
-      {
-        name: "JustDance 2023",
-        imageUrl: JDPicture, // replace with actual image URL
-        websiteUrl: "https://www.ubisoft.com/en-gb/game/just-dance/2023" // replace with actual website URL
-      }
+        {
+            name: "Oddballers",
+            imageUrl: ODDBALLERS, // replace with actual image URL
+            websiteUrl: "https://www.ubisoft.com/en-gb/game/oddballers" // replace with actual website URL
+        },
+        {
+            name: "JustDance 2023",
+            imageUrl: JDPicture, // replace with actual image URL
+            websiteUrl: "https://www.ubisoft.com/en-gb/game/just-dance/2023" // replace with actual website URL
+        }
     ];
-  </script>
+</script>
 
 <style>
     div {
-      background-color: var(--bg-color);
+        background-color: var(--bg-color);
     }
-  </style>
-  
-  <h1 class="text-4xl font-bold text-center col-span-4 text-white  p-4">Games I've Worked On</h1>
-  <div class="grid grid-cols-1 md:grid-cols-4 gap-8 p-8 justify-center items-center">
-    {#each games as { name, imageUrl, websiteUrl }}
-      <div class="transform transition-transform duration-300 hover:scale-105 bg-white shadow-lg hover:bg-gray-100 hover:shadow-xl rounded-lg overflow-hidden max-w-sm">
-        <a href={websiteUrl} target="_blank" rel="noopener noreferrer">
-          <img src={imageUrl} alt={name} class="w-full h-64 object-cover" />
-        </a>
-        <div class="p-4">
-          <a href={websiteUrl} target="_blank" rel="noopener noreferrer" class="text-lg font-medium text-blue-500 hover:text-blue-700">
-            {name}
-          </a>
+    html, body {
+        height: 100%;
+        margin: 0;
+    }
+    .iframe-container {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 16px;
+        justify-content: center;
+    }
+
+    .iframe-container iframe {
+        flex: 1 1 calc(50% - 16px); /* Adjust the width as needed */
+        max-width: 552px;
+        height: 167px;
+    }
+</style>
+
+
+<div class="bg-gradient-to-r from-gray-700 via-gray-900 to-black dark:bg-gray-900">
+<h1 class="text-4xl font-bold text-center col-span-4 text-white  p-4">Games I've Worked On</h1>
+<div class="grid grid-cols-1 md:grid-cols-4 gap-8 p-8 justify-center items-center">
+    {#each games as {name, imageUrl, websiteUrl}}
+        <div class="transform transition-transform duration-300 hover:scale-105 bg-white shadow-lg hover:bg-gray-100 hover:shadow-xl rounded-lg overflow-hidden max-w-sm">
+            <a href={websiteUrl} target="_blank" rel="noopener noreferrer">
+                <img src={imageUrl} alt={name} class="w-full h-64 object-cover"/>
+            </a>
+            <div class="p-4">
+                <a href={websiteUrl} target="_blank" rel="noopener noreferrer"
+                   class="text-lg font-medium text-blue-500 hover:text-blue-700">
+                    {name}
+                </a>
+            </div>
         </div>
-      </div>
     {/each}
-  </div>
-  
+</div>
+
+<h2 class="text-3xl font-bold text-center col-span-4 text-white p-4">My Itch.io Portfolio</h2>
+<div class="flex justify-center p-8">
+    <a href="https://jaibeer72.itch.io" target="_blank" rel="noopener noreferrer"
+       class="text-lg font-medium text-blue-500 hover:text-blue-700">
+        Visit my itch.io portfolio
+    </a>
+</div>
+
+<div class="iframe-container">
+    <iframe frameborder="0" src="https://itch.io/embed/299649?dark=true" width="552" height="167"><a href="https://suryavamsikk.itch.io/de-light">De-light by suryavamsikk, jaibeer72, bustingjam, Tarun.b</a></iframe>
+    <iframe frameborder="0" src="https://itch.io/embed/402744?dark=true" width="552" height="167"><a href="https://jaibeer72.itch.io/escapeforever">EscapeForever by jaibeer72</a></iframe>
+    <iframe frameborder="0" src="https://itch.io/embed/405868?dark=true" width="552" height="167"><a href="https://jaibeer72.itch.io/hungry-harold-ar">Hungry Harold AR by jaibeer72</a></iframe>
+    <iframe frameborder="0" src="https://itch.io/embed/304897?dark=true" width="552" height="167"><a href="https://jaibeer72.itch.io/openglproceduralgeneratedlandmass">OpenGLProceduralGeneratedLandMass by jaibeer72</a></iframe>
+    <iframe frameborder="0" src="https://itch.io/embed/304822?dark=true" width="552" height="167"><a href="https://jaibeer72.itch.io/visualiserio">VisualiserIO by jaibeer72</a></iframe>
+</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,64 +9,70 @@
     let card: HTMLElement;
 
     function handleInteraction(event: MouseEvent | TouchEvent) {
-    let offsetX, offsetY, width, height;
+        let offsetX, offsetY, width, height;
 
-    if (event instanceof MouseEvent) {
-      offsetX = event.offsetX;
-      offsetY = event.offsetY;
-    } else {
-      const rect = (event.target as HTMLElement).getBoundingClientRect();
-      offsetX = event.touches[0].clientX - rect.left;
-      offsetY = event.touches[0].clientY - rect.top;
+        if (event instanceof MouseEvent) {
+            offsetX = event.offsetX;
+            offsetY = event.offsetY;
+        } else {
+            const rect = (event.target as HTMLElement).getBoundingClientRect();
+            offsetX = event.touches[0].clientX - rect.left;
+            offsetY = event.touches[0].clientY - rect.top;
+        }
+
+        ({width, height} = card.getBoundingClientRect());
+        let xRotation = (offsetY / height - 0.5) * 20;
+        let yRotation = -(offsetX / width - 0.5) * 20;
+        card.style.transform = `perspective(500px) rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
     }
 
-    ({ width, height } = card.getBoundingClientRect());
-    let xRotation = (offsetY / height - 0.5) * 20;
-    let yRotation = -(offsetX / width - 0.5) * 20;
-    card.style.transform = `perspective(500px) rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
-  }
-
-  function handleMouseLeave() {
-    card.style.transform = '';
-  }
+    function handleMouseLeave() {
+        card.style.transform = '';
+    }
 </script>
-<section class="flex flex-wrap justify-center items-center text-center w-full h-screen bg-gradient-to-r from-purple-400 to-pink-600">
-    <div 
-    bind:this={card} 
-    on:mousemove={handleInteraction} 
-    on:touchmove={handleInteraction} 
-    on:mouseleave={handleMouseLeave} 
-    role="region" 
-    aria-label="3D interactive card" 
-    class="py-4">
-    <div class="w-full max-w-sm bg-gradient-to-r from-gray-700 via-gray-900 to-black border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-11">
-        <div class="flex flex-col items-center pb-1">
-            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={profilePicture} alt="Bonnie image"/>
-            <h5>Jaibeer Singh Dugal</h5>
-            <span class="text-sm text-gray-500 dark:text-gray-400">Software Developer</span>
-            <div class="flex mt-4 space-x-3 md:mt-6">
-                <button type="button" class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
-                    on:click={redirectGithub}
-                >
-                    <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                      <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
-                    </svg>
-                    Check out my Github
-                </button>
+<div class="bg-gradient-to-r from-gray-700 via-gray-900 to-black dark:bg-gray-900">
+    <section
+            class="flex flex-wrap justify-center items-center text-center w-full bg-gradient-to-r from-purple-400 to-pink-600">
+        <div
+                bind:this={card}
+                on:mousemove={handleInteraction}
+                on:touchmove={handleInteraction}
+                on:mouseleave={handleMouseLeave}
+                role="region"
+                aria-label="3D interactive card"
+                class="py-4">
+            <div class="w-full max-w-sm bg-gradient-to-r from-gray-700 via-gray-900 to-black border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-11">
+                <div class="flex flex-col items-center pb-1">
+                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={profilePicture} alt="Bonnie image"/>
+                    <h5>Jaibeer Singh Dugal</h5>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Software Engineer</span>
+                    <div class="flex mt-4 space-x-3 md:mt-6">
+                        <button type="button"
+                                class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
+                                on:click={redirectGithub}
+                        >
+                            <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                 fill="currentColor" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd"
+                                      d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                                      clip-rule="evenodd"/>
+                            </svg>
+                            Check out my Github
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
-    </div> 
-</div>
+    </section>
     <div class="rounded-lg w-full h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black">
-        <GamesWorkedOn />
-             <!-- Play this game basically here in this iframe -->
-    <div class ="">
-      <h1 class="text-4xl font-bold text-center col-span-4 text-white  p-4">Play Space bound here for fun while I slowly add more of my experiance and projects</h1>
-      <iframe
-      src="https://jaibeer72.github.io/spacebound-html-game/"
-      frameborder="0"
-      style="width: 100%; height: 700px"
-    ></iframe>
-  </div>
+        <GamesWorkedOn/>
+        <div class="rounded-lg w-full h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black">
+            <h1 class="text-4xl font-bold text-center col-span-4 text-white p-4">Play Space bound here for fun while I slowly add more of my experience and projects</h1>
+            <iframe
+                    src="https://jaibeer72.github.io/spacebound-html-game/"
+                    frameborder="0"
+                    style="width: 100%; height: 700px"
+            ></iframe>
+        </div>
     </div>
- </section>
+</div>
